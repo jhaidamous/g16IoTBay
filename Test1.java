6dc447f88a6b346fcb367f056d173629c11074e0
Jonathan <jonathan.m.haidamous@student.uts.edu.au>
2022-03-02 15:16:16 +1100

    test

---
+++ Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public static void main(String[] args) {
+    int[] testarray = {1, 2, 3, 4, 5};
+
+    for (int testloop : testarray) {
+        System.out.println(testloop);
+    }
+
+
+    }
+}
